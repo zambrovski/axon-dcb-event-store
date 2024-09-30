306893298535eb1552758a3ca0ea2ed741ca0a9d
Simon Zambrovski <simon.zambrovski@holisticon.de>
2024-09-30 18:05:00 +0200

    port to 21, implement rename handler

--- sample-faculty/src/main/java/io/event/thinking/sample/faculty/commandhandler/RenameCourseCommandHandler.java
+++ sample-faculty/src/main/java/io/event/thinking/sample/faculty/commandhandler/RenameCourseCommandHandler.java
@@ -1,44 +1,135 @@
 package io.event.thinking.sample.faculty.commandhandler;
 
 import io.event.thinking.eventstore.api.Criteria;
+import io.event.thinking.eventstore.api.Criterion;
 import io.event.thinking.micro.es.DcbCommandHandler;
 import io.event.thinking.micro.es.Event;
 import io.event.thinking.sample.faculty.api.command.RenameCourse;
+import io.event.thinking.sample.faculty.api.event.CourseCreated;
+import io.event.thinking.sample.faculty.api.event.CourseRenamed;
 
 import java.util.List;
+import java.util.function.Function;
+
+import static io.event.thinking.eventstore.api.Criteria.anyOf;
+import static io.event.thinking.eventstore.api.Criterion.allOf;
+import static io.event.thinking.micro.es.Event.event;
+import static io.event.thinking.micro.es.Indices.typeIndex;
+import static io.event.thinking.sample.faculty.commandhandler.FacultyIndices.courseIdIndex;
 
 public class RenameCourseCommandHandler implements DcbCommandHandler<RenameCourse, RenameCourseCommandHandler.State> {
 
-    /*
-        Filter for events, that state whether
-        - this course has been created
-        - this course has been renamed
-     */
-    @Override
-    public Criteria criteria(RenameCourse command) {
-        // TODO: Build criteria
-        return null;
+  @Override
+  public Criteria criteria(RenameCourse renameCourse) {
+    // composed of state criteria
+    return anyOf(
+        NoCourseCreatedState.stateCriteria().apply(renameCourse),
+        NamedCourseState.stateCriteria().apply(renameCourse)
+    );
+  }
+
+  @Override
+  public State initialState() {
+    return State.initial();
+  }
+
+  @Override
+  public State source(Object event, State state) {
+    // could go to framework and do dynamic dispatch from there based on sealed state
+    return switch (state) {
+      case NoCourseCreatedState s -> s.evolve(event);
+      case NamedCourseState s -> s.evolve(event);
+      default -> throw new IllegalStateException("No handler for this event");
+    };
+  }
+
+  @Override
+  public List<Event> handle(RenameCourse command, State state) {
+    // could go to framework and do dynamic dispatch from there based on sealed state
+    return switch (state) {
+      case NoCourseCreatedState s -> s.handle(command);
+      case NamedCourseState s -> s.handle(command);
+      default -> throw new IllegalStateException("Invalid state");
+    };
+  }
+
+  public interface State {
+
+    <T extends State> T evolve(Object event);
+
+    List<Event> handle(RenameCourse command)
+
+    static NoCourseCreatedState initial() {
+      return new NoCourseCreatedState();
+    }
+  }
+
+  /**
+   * Initial state (no course created)
+   */
+  public record NoCourseCreatedState() implements State {
+
+    public static Function<Object, Criterion> stateCriteria() {
+      // this student subscribed to this course
+      return (o) -> {
+        RenameCourse renameCourse = (RenameCourse) o;
+        return allOf(typeIndex(CourseCreated.NAME),
+            courseIdIndex(
+                renameCourse.courseId() // TAG deduction from the command
+            )
+        );
+      };
     }
 
     @Override
-    public State initialState() {
-        // TODO: Construct initial state
-        return new State();
+    public State evolve(Object event) {
+      if (event instanceof CourseCreated) {
+        return new NamedCourseState(((CourseCreated) event).name());
+      }
+      throw new RuntimeException("Can't evolve using " + event);
+    }
+
+    public List<Event> handle(RenameCourse command) {
+      throw new RuntimeException("Course does not exist");
+    }
+
+  }
+
+  public record NamedCourseState(String name) implements State {
+
+    public static Function<Object, Criterion> stateCriteria() {
+      // this student subscribed to this course
+      return (o) -> {
+        RenameCourse renameCourse = (RenameCourse) o;
+        return allOf(
+            typeIndex(CourseRenamed.NAME),
+            courseIdIndex(
+                renameCourse.courseId()
+            )
+        );
+      };
     }
 
     @Override
-    public State source(Object event, State state) {
-        // TODO: Source state from events
-        return state;
+    public State evolve(Object event) {
+      if (event instanceof CourseRenamed) {
+        return new NamedCourseState(((CourseRenamed) event).newName());
+      }
+      throw new RuntimeException("Can't evolve using " + event);
     }
 
-    @Override
-    public List<Event> handle(RenameCourse command, State state) {
-        // TODO: Handle the command
-        return List.of();
+    public List<Event> handle(RenameCourse command) {
+      if (this.name.equals(command.newName())) {
+        throw new RuntimeException("Course already has the name " + name);
+      }
+      CourseRenamed payload = new CourseRenamed(command.courseId(), command.newName());
+      return List.of(
+          event(
+              payload,
+              typeIndex(CourseRenamed.NAME),
+              courseIdIndex(command.courseId())
+          )
+      );
     }
-
-    public record State() {
-        // TODO: Implement the state
-    }
+  }
 }
